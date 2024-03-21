690f1371f5769b977e25b176eb33dcad9b13f55a
ErMuthii <er.muthii@gmail.com>
2024-03-21 19:44:48 +0300

    done

--- server/models.py
+++ server/models.py
@@ -13,6 +13,21 @@ class Author(db.Model):
 
     # Add validators 
 
+    @validates('name')
+    def validate_name(self,key,name):
+        if name == '': 
+            raise ValueError("Name cannot be empty.")
+        return name
+    
+    @validates('phone_number')
+    def validates_phone_number(self,key,phone_number):
+        if len(phone_number) != 10:
+            raise ValueError("Phone number must be ten digits.")
+        return phone_number
+    
+
+        
+    
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
 
@@ -28,7 +43,25 @@ class Post(db.Model):
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     # Add validators  
-
+    @validates('title')
+    def validates_title(self,key,title):
+        if title == '':
+            raise ValueError("Post must have a title.")
+        return title
+    
+    @validates('content')
+    def validate_content(self,key,content):
+        if len(content) > 250:
+            raise ValueError("Content must be atleast 250 digits long.")
+        return content
+    
+    @validates('category')
+    def validates_category(self,key,category):
+        if category not in ['Fiction', 'Non-Fiction']:
+            raise ValueError("Category must be Fiction or Non Fiction.")
+        return category
+    
+    
 
     def __repr__(self):
         return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
